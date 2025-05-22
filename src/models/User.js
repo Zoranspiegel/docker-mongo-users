39636b15e54800015e452a32c7bee5619555e7c5
Juan José Rodriguez <zoranbow@gmail.com>
2025-05-22 14:31:04 -0500

    User model & error handler middleware

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  avatar: {
+    type: String,
+    required: false,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
